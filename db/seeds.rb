cdb7627b30f6c821d556e456c4f2c65cdc60dee6
Norman Cantoral <toliveanddie76@gmail.com>
2018-04-22 07:01:43 +0000

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create!(
+  email: '111@aaa.com',
+  password: 'kelley22..',
+  password_confirmation: 'kelley22..')
+
+
+  
